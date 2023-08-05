b1db4b97304b26b9a24dfcfe5bffbbf5ae3805e0
Jason G <jasondouglasgibbs@gmail.com>
2023-08-04 23:24:30 -0400

    Test

--- main.R
+++ main.R
@@ -8,8 +8,10 @@ library(tictoc)
 library(conflicted)
 library(plotly)
 library(jsonlite)
+library(TAF)
 wd<-getwd()
 
+tic("Read in data and coerce.")
 FitBitZip<-grep("takeout", list.files(file.path(wd,"FitBit")), value=TRUE)
 FitBitZipPath<-file.path(wd,"FitBit",FitBitZip)
 ##Read in manual blood pressure and heart rate data.##
@@ -22,7 +24,6 @@ zipped_heart_rate <- grep(paste0("Takeout/","FitBit/","Global Export Data/",'hea
                          ignore.case=TRUE, value=TRUE)
 unzip(FitBitZipPath, files=zipped_heart_rate, exdir = file.path(wd, "FitBit","FitBitHeartRate"), overwrite = TRUE)
 HeartRateList<-list.files(path=wd, pattern = "heart_rate", recursive = TRUE)
-
 
 for(i in 1:length(HeartRateList)){
   if(i==1){
@@ -37,10 +38,10 @@ for(i in 1:length(HeartRateList)){
 HeartRateDF$dateTime<-as.POSIXct(HeartRateDF$dateTime, format="%m/%d/%y %H:%M:%S", tz="UTC")
 HeartRateDF$dateTime<-format(HeartRateDF$dateTime,tz="America/New_York", usetz = TRUE)
 HeartRateDF$dateTime<-as.POSIXct(HeartRateDF$dateTime, tz="America/New_York")
+toc()
 
 
-
-
+tic("Plotting")
 HeartRatePlot<-ggplot(HeartRateDF, aes(x=dateTime, y=value.bpm))+
   geom_point(size = 0.1, color = "orange2", alpha = 0.5) +
   geom_smooth(method = 'gam', formula = y ~ s(x, k = 10, bs = "cs"),
